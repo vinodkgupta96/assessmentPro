626c7ac62d3ebd3c9c8c96b31d97c741453cf27e
vinodkgupta96 <vinodkgupta.1996@gmail.com>
2019-06-26 19:51:35 +0530

    Initial commit for advertising.

---
+++ assessmentpro-advertising/src/main/java/com/assessment/assessmentproadvertising/AssessmentproAdvertisingApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.assessmentproadvertising;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentproAdvertisingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentproAdvertisingApplication.class, args);
+	}
+
+}
